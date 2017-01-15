d1a8345740f47d19557b900cc2d645ed3ddffedb
Ziga Krasovec <zkrasovec@gmail.com>
2017-01-15 19:54:10 +0100

    Fixed main menu on all pages

--- wordpress/wp-content/themes/podhalanskiOvcar/header.php
+++ wordpress/wp-content/themes/podhalanskiOvcar/header.php
@@ -1,30 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="width" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo get_bloginfo('title'); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
   <nav class="main-nav">
       <div class="logo">
+        <a href="<?php echo get_bloginfo('url'); ?>">
           <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
+        </a>
       </div>
-      <?php if(have_rows('main_menu')): ?>
-        <div class="menu">
-          <ul>
-            <?php while(have_rows('main_menu')): the_row();
-              //Vars
-              $item_id = implode('-', explode(' ', get_sub_field('link_label')));
-            ?>
-            <li class="nav-btn" id="<?php echo $item_id ?>">
-              <a href="<?php echo get_sub_field('link_value'); ?>"><?php echo get_sub_field('link_label') ?></a>
-            </li>
-            <li> | </li>
-            <?php endwhile; ?>
-          </ul>
-        </div>
-      <?php endif; ?>
+      <?php wp_nav_menu(); ?>
   </nav>
 
 <?php ?>
